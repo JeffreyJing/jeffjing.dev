606b62c3080eeb4a66a5903210687d69484c57ff
Jeffrey Jing <jjingdn@gmail.com>
2025-06-11 17:29:17 -0700

    fixed form handler

--- terraform/form_lambda/form_handler.py
+++ terraform/form_lambda/form_handler.py
@@ -51,7 +51,7 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200,
             'headers': {
-                "Access-Control-Allow-Origin": "http://localhost:5173",
+                "Access-Control-Allow-Origin": "https://jeffjing.dev",
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Methods": "POST, OPTIONS"
             },
